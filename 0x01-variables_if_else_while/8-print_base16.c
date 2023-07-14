5aed6a8880fcea638bf5d08fdcbf235d229e32fa
eslam <eslam@gmail.com.com>
2023-07-13 22:37:55 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,19 +5,23 @@
 /**
  * main - Entry point
  *
- * Description:  program that prints the lowercase alphabet in reverse
+ * Description:  program that prints the all  numbers of base 16 in lowercase
  *
  * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
-	char ch = 'z';
+	int i = 48;
 
-	while (ch >= 'a')
-	{
-		putchar(ch);
-		ch--;
+	while (i <= 102)
+	}
+		putchar(i);
+
+
+		if (i == 57)
+			i += 39;
+		i++;
 	}
 	putchar('\n');
 
