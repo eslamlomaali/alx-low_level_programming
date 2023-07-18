13d5253c74167ff5a47d964a12018a9995c47224
eslam <eslam@gmail.com.com>
2023-07-17 21:02:47 -0700

    finish

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ *print_alphabet -  prints the alphabet, in lowercase
+ *
+*/
+
+void print_alphabet(void)
+{
+	int cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+		_putchar(cha);
+	_putchar('\n');
+}
