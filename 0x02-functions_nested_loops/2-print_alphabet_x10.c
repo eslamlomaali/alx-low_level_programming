13e07180301dff8e676e2465376b51ea9a49c08c
eslam <eslam@gmail.com.com>
2023-07-17 21:16:32 -0700

    finish

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ *2-print_alphabet_x10.c - prints 10 times the alphabet, in lowercase
+ *
+*/
+
+void print_alphabet_x10(void);
+{
+	int rows, cha;
+
+	for (rows = 0; rows <= 9; rows++)
+	{
+		for (cha = 'a'; cha <= 'z'; cha++)
+			_putchar(cha);
+		_putchar('\n');
+	}
+}
+
