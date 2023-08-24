ca92e254489952743491a8301cdf7a9c4f367954
eslam <eslam@gmail.com>
2023-08-24 07:26:20 -0700

    done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+  * add_node -Write a function that adds a new node at the end of list_t list
+  * @head: Main linked list
+  * @str: The string
+  *
+  * Return: The address
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t p;
+
+	if (head && str)
+	{
+		p = malloc(sizeof(list_t));
+		if (p == NULL)
+			return (NULL);
+
+		p->str = strdup(str);
+		p->len = _strlen(str);
+		p->next = *head;
+
+		*head = p;
+
+		return (p);
+	}
+
+	return (0);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	for (; *s; ++s, i++)
+	{
+		return (i);
+	}
+}
