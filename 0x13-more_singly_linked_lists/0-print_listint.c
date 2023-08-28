bdc4dfb7a581d93df072de630f97d6ceccae22ed
eslam <eslam@gmail.com>
2023-08-28 08:37:04 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t c = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		c++;
+		i++;
 	}
-	return (c);
+	return (i);
 }
