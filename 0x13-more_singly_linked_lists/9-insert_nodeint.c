9bd749465e2e03cbe408d6ef78879b40b54883a6
eslam <eslam@gmail.com>
2023-08-28 09:39:40 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				if (c == idx)
 				{
-					node->next =node2->next;
+					node->next = node2->next;
 					node2->next = node;
 					return (node);
 				}
