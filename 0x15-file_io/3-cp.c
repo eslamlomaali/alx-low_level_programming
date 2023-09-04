b316a8664be9a57dd0efad63a1386c4aa8bd623d
eslam <eslam@gmail.com>
2023-09-04 02:20:39 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,10 +42,10 @@ while (n == 1024)
 }
 
 if (close(of) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", of), exit(100);
 
 if (close(nf) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", nf), exit(100);
 
 return (0);
 }
